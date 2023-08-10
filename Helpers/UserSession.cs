f0b720543ed977d8a680fced13368659279a0648
michaelmondong <95898805+michaelmondong@users.noreply.github.com>
2023-08-10 15:09:54 +0700

    Add files via upload

---
+++ Helpers/UserSession.cs
@@ -0,0 +1,29 @@
+﻿namespace SAHARA2.Helpers
+{
+    internal class UserSession
+    {
+        public static string Username { get; private set; }
+        public static string Role { get; private set; }
+
+        public static bool IsLoggedIn { get; private set; }
+
+        public static void SetUser(string username, string role)
+        {
+            Username = username;
+            Role = role;
+            IsLoggedIn = true;
+        }
+
+        public static void ClearUser()
+        {
+            Username = null;
+            Role = null;
+            IsLoggedIn = false;
+        }
+
+        internal static void SetToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
